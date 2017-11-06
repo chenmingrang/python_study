8d2318a4b9e9597429852c4caa38d17eda4b6924
chenmingrang <chenmingrang@126.com>
2017-11-06 22:15:06 +0800

    flask

---
+++ python_start/web_develop/MVC_demo.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/signin', methods=['GET'])
+def signin_from():
+    return render_template('form.html')
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'password':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or Password', username=username)
+
+if __name__ == '__main__':
+    app.run()
+
+
+"""
+Flask 会在 templates 文件夹里寻找模板
+所以，如果你的应用是个模块，这个文件夹应该与模块同级；如果它是一个包，那么这个文件夹作为包的子目录
+"""
--- python_start/web_develop/WSGI_demo1.py
+++ python_start/web_develop/WSGI_demo1.py
@@ -11,7 +11,7 @@ def simple_app(environ, start_response):
 
     start_response(status, headers)
 
-    ret = [("%s: %s\n" % (key, value)).encode("utf-8")
+    ret = [("%s = %s\n" % (key, value)).encode("utf-8")
            for key, value in environ.items()]
     print(len(ret))
 
@@ -20,3 +20,9 @@ def simple_app(environ, start_response):
 httpd = make_server('', 8000, simple_app)
 print("Serving on port 8000...")
 httpd.serve_forever()
+
+
+"""
+无论多么复杂的Web应用程序，入口都是一个WSGI处理函数。
+HTTP请求的所有输入信息都可以通过environ获得，HTTP响应的输出都可以通过start_response()加上函数返回值作为Body。
+"""
