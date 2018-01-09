5ff8f0fb6630a8e90568509edec5acbb2f7756bf
chenmingrang <chenmingrang@126.com>
2018-01-09 14:24:07 +0800

    socket

--- study_further/python_net_programing/socket/socket_demo1.py
+++ study_further/python_net_programing/socket/socket_demo1.py
@@ -9,3 +9,8 @@ s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #发消息
 send_addr = ("192.168.238.1", 8080)
 s_udp.sendto(b"hello world", send_addr)
+
+rec_data = s_udp.recvfrom(1024)
+print('receive "%s" from %s:%s' % (str(rec_data[0], "utf-8"), rec_data[1][0], rec_data[1][1]))
+
+s_udp.close()
